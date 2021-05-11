7fe39f54b634045baa46e75dd3d4728bae892176
Luiz Henrique A. Vigiato <lh.vigiato@gmail.com>
2021-05-10 23:17:35 -0300

    removendo chamadas desnecessarias

--- front-react/src/pages/_routes.jsx
+++ front-react/src/pages/_routes.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import {  BrowserRouter, HashRouter, Switch, Route } from "react-router-dom";
+import {  BrowserRouter, Switch, Route } from "react-router-dom";
 import ScrollToTop from './ScrollTop'
 
 import Menu from "./cardapio";
