d1d69f70af70c071d2dfb282bd11bc48dcd81e36
Luiz Henrique A. Vigiato <lh.vigiato@gmail.com>
2021-05-10 23:18:01 -0300

    adicao e correcao

--- front-react/src/pages/itenscardapio.jsx
+++ front-react/src/pages/itenscardapio.jsx
@@ -1,5 +1,5 @@
-import React from "react";
-import { makeStyles } from "@material-ui/core/styles";
+import React, { Component } from "react";
+// import { makeStyles } from "@material-ui/core/styles";
 import Card from "@material-ui/core/Card";
 import CardActionArea from "@material-ui/core/CardActionArea";
 import CardActions from "@material-ui/core/CardActions";
@@ -10,59 +10,67 @@ import Typography from "@material-ui/core/Typography";
 import Box from '@material-ui/core/Box';
 
 import bacon from "../public/xbacon.jpg";
+import { withStyles } from "@material-ui/styles";
 
-const useStyles = makeStyles({
+const styles = theme =>({
   root: {
     maxWidth: 460,
-       
+
   },
   action: {
     backgroung: "#F000FF",
-    color: "#FF0000",
+    color: "#d50000",
     marginBottom: 3
   }
 });
 
+// const classes = useStyles();
 
 
+// const ItensCardapio = () => {
 
-const ItensCardapio = () => {
+class ItensCardapio extends Component {
 
-    const classes = useStyles();
+  render() {
 
+    const { classes } = this.props;
+    
     return (
+
       <Box mb={2}>
         <Card className={classes.root}>
-        <CardActionArea>
-          <CardMedia
-            component="img"
-            alt="X-bacon"
-            height="140"
-            image={bacon}
-            title="X-bacon"
-          />
-          <CardContent>
-            <Typography gutterBottom variant="h5" component="h2">
-              X-Burger Bacon Plus
+          <CardActionArea>
+            <CardMedia
+              component="img"
+              alt="X-bacon"
+              height="140"
+              image={bacon}
+              title="X-bacon"
+            />
+            <CardContent>
+              <Typography gutterBottom variant="h5" component="h2">
+                X-Burger Bacon Plus
             </Typography>
-            <Typography variant="body2" color="textSecondary" component="p">
-              120 Gramas de costela defiada, 20g de bacon, Pão de hamburger,
-              alface, tomate, ovo, queijo cheddar, cebola caramelizada.
+              <Typography variant="body2" color="textSecondary" component="p">
+                120 Gramas de costela defiada, 20g de bacon, Pão de hamburger,
+                alface, tomate, ovo, queijo cheddar, cebola caramelizada.
             </Typography>
-          </CardContent>
-        </CardActionArea>
-        <CardActions className={classes.action}>
-          <Typography>
-            <b>Valor R$ 25,00</b>
-          </Typography>
-  
-          <Button className={classes.bottonLeft} size="small" color="primary">
-            Learn More
+            </CardContent>
+          </CardActionArea>
+          <CardActions className={classes.action}>
+            <Typography>
+              <b>Valor R$ 25,00</b>
+            </Typography>
+
+            <Button className={classes.bottonLeft} size="small" color="primary">
+              Learn More
           </Button>
-        </CardActions>
-      </Card>
+          </CardActions>
+        </Card>
       </Box>
     );
-  };
-  
-  export default ItensCardapio;
+  }
+}
+
+
+export default withStyles(styles)(ItensCardapio);
--- front-react/src/text_teste/itensMenu.jsx
+++ front-react/src/text_teste/itensMenu.jsx
@@ -1,23 +1,32 @@
+import bacon from "../public/xbacon.jpg";
+import egg from "../public/xegg.jpg";
+import coca from "../public/coca.jpg";
+import heineken from "../public/heineken.jpg";
+
 const Itens = [
     {
         label: "X-Burger Bacon Plus",
         descricao: "120 Gramas de costela defiada, 20g de bacon, Pão de hamburger, alface, tomate, ovo, queijo cheddar, cebola caramelizada.",
-        valor: "25,00"
+        valor: "25,00",
+        imagem: bacon
     },
     {
         label: "X-Burger Ovo Plus",
         descricao: "Pão de hamburger, tomate, alface, 2 ovos, queijo mussarela, cebola caramelizada",
-        valor: "15,00"
+        valor: "15,00",
+        imagem: egg
     },
     {
         label: "Coca Cola 2L",
         descricao: "Aquela coca cola gelada, tricando, mais gelada que coração de Ex.",
-        valor: "10,00"
+        valor: "10,00",
+        imagem: coca
     },
     {
-        label: "Coca Cola 2L",
-        descricao: "Aquela coca cola gelada, tricando, mais gelada que coração de Ex.",
-        valor: "10,00"
+        label: "Heineken 600ml",
+        descricao: "Para aliviar seu estresse do dia, temos esta estupenda cerveja pra lá de gelada.",
+        valor: "10,00",
+        imagem: heineken
     }
 ];
 
