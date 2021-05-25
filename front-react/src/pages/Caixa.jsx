fafdb422d0b6d0f8c19957d2568952e3a3661b65
Luiz Henrique A. Vigiato <lh.vigiato@gmail.com>
2021-05-25 12:41:18 -0300

    correção de localizaçao do codigo cliente

--- front-react/src/pages/Caixa.jsx
+++ front-react/src/pages/Caixa.jsx
@@ -36,10 +36,20 @@ const useStyles = makeStyles((theme) => ({
         backgroundColor: '#eeeeee'
     },
     btn: {
-        marginTop: theme.spacing(5),
+        marginTop: theme.spacing(3),
         height: theme.spacing(6),
         marginRight: theme.spacing(2)
     },
+    btn_pesquisar: {
+        marginTop: theme.spacing(0),
+    },
+    pesquisar: {
+        width: 300
+    },
+    input_pesquisar: {
+        width: 130,
+        marginRight: theme.spacing(1)
+    }
 
 }));
 
@@ -108,83 +118,92 @@ export default function Caixa() {
                     <Table>
                         <TableHead>
                             <TableRow >
-                                <TableCell className={classes.topo} aling="center" colSpan={4}>
+                                <TableCell className={classes.topo} aling="center" colSpan={3}>
                                     <b><ReceiptIcon className={classes.desenho} />Caixa</b>
                                 </TableCell>
+                                <TableCell className={classes.pesquisar} align="right">
+                                    <Grid container alignItems="center" align="right" spacing={0}>
+                                        <Grid container item md={6} sm={6} xs={6} xl={6} lg={6}>
+                                            <TextField className={classes.input_pesquisar} required id="codigo" label="Cod. Cliente" variant="outlined" size="small" />
+                                        </Grid>
+                                        <Grid container item md={6} sm={6} xs={6} xl={6} lg={6}>
+                                        <Button className={classes.btn_pesquisar} variant="contained" color="primary" size="small" onClick={event => window.location.href = '/'}>
+                                            Pesquisar
+                                        </Button>
+                                        </Grid>
+                                    </Grid>
+                                </TableCell>
                             </TableRow>
-                            <TableRow>
-                                <TableCell>Item</TableCell>
-                                <TableCell>Qnt.</TableCell>
-                                <TableCell>Valor Un.</TableCell>
-                                <TableCell aling="right">Total p/ Item</TableCell>
-                            </TableRow>
+                        <TableRow>
+                            <TableCell>Item</TableCell>
+                            <TableCell>Qnt.</TableCell>
+                            <TableCell>Valor Un.</TableCell>
+                            <TableCell align="right">Total p/ Item</TableCell>
+                        </TableRow>
                         </TableHead>
-                        <TableBody>
-                            {carrinho.map((item, key) => (
-                                <TableRow key={item.id}>
-                                    <TableCell>{item.label}</TableCell>
-                                    <TableCell>{item.quant}</TableCell>
-                                    <TableCell>{(item.valor).toFixed(2)}</TableCell>
-                                    <TableCell>{(sumLinha(item.valor, item.quant)).toFixed(2)}</TableCell>
-                                </TableRow>
-                            ))}
-
-                            <TableRow className={classes.total}>
-                                <TableCell rowSpan={3} />
-                                <TableCell colSpan={2}>Total</TableCell>
-                                <TableCell aling="right">{(valorTotal()).toFixed(2)}</TableCell>
+                    <TableBody>
+                        {carrinho.map((item, key) => (
+                            <TableRow key={item.id}>
+                                <TableCell>{item.label}</TableCell>
+                                <TableCell>{item.quant}</TableCell>
+                                <TableCell>{(item.valor).toFixed(2)}</TableCell>
+                                <TableCell align="right">{(sumLinha(item.valor, item.quant)).toFixed(2)}</TableCell>
                             </TableRow>
-                        </TableBody>
+                        ))}
+
+                        <TableRow className={classes.total}>
+                            <TableCell rowSpan={3} />
+                            <TableCell colSpan={2}>Total</TableCell>
+                            <TableCell align="right">{(valorTotal()).toFixed(2)}</TableCell>
+                        </TableRow>
+                    </TableBody>
                     </Table>
                 </TableContainer>
 
-                <Grid container alignItems="center" spacing={0}>
-                    <Grid container item md={6} sm={6} xs={6} xl={3} lg={2}>
-                        <TextField className={classes.input} required id="codigo" label="Cod. Cliente" variant="outlined" size="medium" />
-                    </Grid>
-                    <Grid container item md={6} sm={6} xs={6} xl={3} lg={2}>
-                        <FormControl fullWidth required variant="outlined" className={classes.input} xs={12}>
-                            <InputLabel htmlFor="outlined-age-native-simple">Forma Pag.</InputLabel>
-                            <Select
-                                native
-                                label="Forma Pag."
-                                open={open}
-                                onClose={selectClose}
-                                onOpen={selectOpen}
-                                value={pagamento}
-                                onChange={selectChange}
-                            >
-                                <option aria-label="None" value="" />
-                                <option value={1}>Dinheiro</option>
-                                <option value={2}>Cartão Credito</option>
-                                <option value={3}>Cartão Debito</option>
-                            </Select>
-                        </FormControl>
-                    </Grid>
-                    <Grid container item md={6} sm={6} xs={6} xl={2} lg={2}>
-                        <TextField className={classes.input} required id="recebido" label="Valor Recebido" variant="outlined" size="medium" onChange={alterarValor}
-                            InputProps={{
-                                startAdornment: <InputAdornment position="start">R$</InputAdornment>,
-                            }} />
-                    </Grid>
-                    <Grid container item md={6} sm={6} xs={6} xl={2} lg={2}>
-                        <TextField className={classes.input} id="troco" label="Troco" variant="outlined" size="medium" aria-readonly InputProps={{
-                            startAdornment: <InputAdornment position="start">R$</InputAdornment>,
-                        }}
-                            placeholder='0' />
-                    </Grid>
-                    <Grid container item md={6} sm={6} xs={6} xl={2} lg={2}>
-                        <Button fullWidth className={classes.btn} variant="contained" color="primary" size="large" onClick={event => window.location.href = '/'}>
-                            Confirmar
-                    </Button>
-                    </Grid>
-                    <Grid container item md={6} sm={6} xs={6} xl={2} lg={2}>
-                        <Button fullWidth className={classes.btn} variant="contained" color="secondary" size="large" onClick={event => window.location.href = '/'}>
-                            Cancelar
-                    </Button>
-                    </Grid>
+            <Grid container alignItems="center" spacing={0}>
+                <Grid container item md={6} sm={6} xs={6} xl={3} lg={3}>
+                    <FormControl fullWidth required variant="outlined" className={classes.input} xs={12}>
+                        <InputLabel htmlFor="outlined-age-native-simple">Forma Pag.</InputLabel>
+                        <Select
+                            native
+                            label="Forma Pag."
+                            open={open}
+                            onClose={selectClose}
+                            onOpen={selectOpen}
+                            value={pagamento}
+                            onChange={selectChange}
+                        >
+                            <option aria-label="None" value="" />
+                            <option value={1}>Dinheiro</option>
+                            <option value={2}>Cartão Credito</option>
+                            <option value={3}>Cartão Debito</option>
+                        </Select>
+                    </FormControl>
                 </Grid>
-            </Container>
+                <Grid container item md={6} sm={6} xs={6} xl={2} lg={3}>
+                    <TextField className={classes.input} required id="recebido" label="Valor Recebido" variant="outlined" size="medium" onChange={alterarValor}
+                        InputProps={{
+                            startAdornment: <InputAdornment position="start">R$</InputAdornment>,
+                        }} />
+                </Grid>
+                <Grid container item md={6} sm={6} xs={6} xl={2} lg={2}>
+                    <TextField className={classes.input} id="troco" label="Troco" variant="outlined" size="medium" aria-readonly InputProps={{
+                        startAdornment: <InputAdornment position="start">R$</InputAdornment>,
+                    }}
+                        placeholder='0' />
+                </Grid>
+                <Grid container item md={6} sm={6} xs={6} xl={2} lg={2}>
+                    <Button fullWidth className={classes.btn} variant="contained" color="primary" size="large" onClick={event => window.location.href = '/'}>
+                        Confirmar
+                    </Button>
+                </Grid>
+                <Grid container item md={6} sm={6} xs={6} xl={2} lg={2}>
+                    <Button fullWidth className={classes.btn} variant="contained" color="secondary" size="large" onClick={event => window.location.href = '/'}>
+                        Cancelar
+                    </Button>
+                </Grid>
+            </Grid>
+        </Container>
         </>
     );
 }
